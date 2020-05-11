87c50fefb8e02535b47bf88ec5384546d5f9ce6a
srikant <srikantkharsh@gmail.com>
2020-05-12 01:54:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+// import PostList from './component/PostList'
+import StateWise from './component/StateWise'
+
+function App() {
+  return (
+    <div className="App">
+        {/* <PostList  name= {"srikant"}/> */}
+        <StateWise name= {"Srikant"} />
+    </div>
+  );
+}
+
+export default App;
